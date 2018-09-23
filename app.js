f5a397a2896e02b6a2fbb3ed7336cf737e76b934
edgardoburgos94 <edgardo.burgos94@gmail.com>
2018-09-22 21:33:42 -0500

    Express saludame 1

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+// app.get('/', (req, res) => {
+//   res.send('<h1>HcUy6Re2LLBRtj</h1>');
+// });
+
 app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+  res.send('<h1>Hola desconocido!</h1>')
+})
+
+app.get('/:nombre', (req, res) => {
+  console.log(req)
+  if (req.params ==='undefined') {
+    res.send('<h1>Hola deconocido!</h1>');
+  } else {
+    res.send('<h1>Hola '+req.params.nombre+'!</h1>');
+  }
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
