b12646225da011c2a955eaba67ce66b434f7dfdc
edgardoburgos94 <edgardo.burgos94@gmail.com>
2018-09-22 22:14:40 -0500

    Pares e impares

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
 //
 // app.get('/', (req, res) => {
 //   res.send('<h1>HcUy6Re2LLBRtj</h1>');
@@ -20,8 +17,21 @@ String.prototype.capitalize = function() {
 // })
 
 // Saludame 2
-app.get('/makers/:nombre', (req, res) => {
-  res.send('<h1>Hola '+req.params.nombre.capitalize()+'!</h1>');
-});
+// String.prototype.capitalize = function() {
+//   return this.charAt(0).toUpperCase() + this.slice(1);
+// }
+// app.get('/makers/:nombre', (req, res) => {
+//   res.send('<h1>Hola '+req.params.nombre.capitalize()+'!</h1>');
+// });
+
+// Pares e impares
+app.get('/', (req, res) => {
+  var content = ''
+  for (var i=1; i < 51; i++) {
+    var parrafo = i%2 === 0 ? i+' Soy Par!' : i+" Soy Impar!"
+    content = content + '<p>'+parrafo+'</p>'
+  }
+  res.send(content)
+})
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
